a8382cd83cd83e61aa27f645c9bee2cbec9c426b
EkoBambang <exocuahbaguz@gmail.com>
2016-10-11 13:45:41 +0700

    third commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+import (    "net/http" )
+func main() {    mux := http.NewServeMux()    
+		fs := http.FileServer(http.Dir("public"))   
+		mux.Handle("/", fs)  
+		http.ListenAndServe(":8080", mux) 
+}
